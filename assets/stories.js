3eb41d836eafa66082d670e6f30c450300ebf888
Eryx <geridarmawan22@gmail.com>
2026-01-12 09:06:10 +0700

    Update stories.js

--- assets/stories.js
+++ assets/stories.js
@@ -2,5 +2,5 @@ export default [
   "stop dulu aktivitasnya sebentar, ada pengumuman penting nih buat kamu",
   "selamat! kamu berhasil unlock achievement, kita bareng terus kedepannya wkwk",
   "hadiahnya bukan piring cantik ya, tapi versi aku yang makin hari makin bucin dan gamau jauh dari kamu",
-  "happy monthversary tanggal 12, sayang! makasih udah bertahan, level up terus ya kita! 🤍"
+  "happy monthversary tanggal 12, sayang! makasih udah bertahan, level up terus ya kita! ♥"
 ];
